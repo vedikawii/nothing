8234886fa3933c86451211d721b57fd8ba1dc393
vedikawii <166423726+vedikawii@users.noreply.github.com>
2024-04-10 14:18:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Function to handle button click events
+function selectOption(option) {
+    // Check which option was clicked
+    if (option === 'yes') {
+        // Flash rainbow colors
+        flashRainbowColors(function() {
+            // Redirect to countdown page when 'Yes' is clicked
+            window.location.href = 'countdown.html';
+        });
+    } else if (option === 'no') {
+        // Change text on the "No" button to "You sure?"
+        document.getElementById('no-button').innerText = 'You sure?'; 
+        // Increase font size of "Yes" button
+        var yesButton = document.getElementById('yes-button');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
+        yesButton.style.fontSize = newSize + 'px';
+    } else {
+        // If neither "Yes" nor "No" was clicked, show an alert message
+        alert('Invalid option!');
+    }
+}
+
 // Function to handle bench options
 function selectBenchOption(option) {
     if (option === '2') {
