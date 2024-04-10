0c96ab7f222acf5b21df070614164233a756d781
vedikawii <166423726+vedikawii@users.noreply.github.com>
2024-04-10 14:37:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,26 @@
-// Function to handle button click events
-function selectOption(option) {
-    // Check which option was clicked
-    if (option === 'yes') {
-        // Flash rainbow colors
-        flashRainbowColors(function() {
-            // Redirect to countdown page when 'Yes' is clicked
-            window.location.href = 'countdown.html';
-        });
-    } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
-        yesButton.style.fontSize = newSize + 'px';
-    } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
-        alert('Invalid option!');
-    }
-}
-
-// Function to handle bench options
+// Function to handle button click events on index.html
 function selectBenchOption(option) {
     if (option === '2') {
-        window.location.href = 'page2.html';
+        window.location.href = 'page2.html'; // Redirect to page2.html when '2 vedikas' is clicked
     }
 }
 
-// Function to handle jump squats options
+// Function to handle button click events on page2.html
 function selectSquatsOption(option) {
     if (option === 'yes') {
-        window.location.href = 'page3.html';
-    } else if (option === 'no') {
-        // Handle 'No' option if needed
+        window.location.href = 'page3.html'; // Redirect to page3.html when 'yes' is clicked
     }
 }
 
-// Function to handle liking option
+// Function to handle button click events on page3.html
 function selectLikeOption(option) {
     if (option === 'yes') {
-        window.location.href = 'final.html'; // Redirect to final.html if option is 'yes'
-    } else if (option === 'no') {
-        // Handle 'No' option if needed
+        window.location.href = 'final.html'; // Redirect to final.html when 'yes' is clicked
     }
-} 
-// Function to handle button click events
-function selectOption(option) {
-    // Check which option was clicked
-    if (option === 'yes') {
-        // Flash rainbow colors
-        flashRainbowColors(function() {
-            // Redirect to countdown page when 'Yes' is clicked
-            window.location.href = 'countdown.html';
-        });
-    } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
-        yesButton.style.fontSize = newSize + 'px';
-    } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
-        alert('Invalid option!');
-    }
-} 
+}
 
-// Function to disable option 2 in third page
-function disableOption() {
+// Function to disable 'no' option in page3.html
+function disableNoOption() {
     document.getElementById('no-option').setAttribute('disabled', true);
 }
 
@@ -89,42 +40,3 @@ function flashRainbowColors(callback) {
         }
     }, 2000); // Flash colors for 2 seconds
 }
-
-// Function to display the cat-heart.gif
-function displayCatHeart() {
-    // Clear existing content in the image container
-    document.getElementById('image-container').innerHTML = '';
-    // Get the container where the image will be displayed
-    var imageContainer = document.getElementById('image-container');
-    // Create a new Image element for the cat-heart
-    var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
-    // Set alternative text for the image (for accessibility)
-    catHeartImage.alt = 'Cat Heart';
-    // When the cat-heart image is fully loaded, add it to the image container
-    catHeartImage.onload = function() {
-        imageContainer.appendChild(catHeartImage);
-        // Hide the options container
-        document.getElementById('options').style.display = 'none';
-    };
-}
-
-// Function to display the cat.gif initially
-function displayCat() {
-    // Get the container where the image will be displayed
-    var imageContainer = document.getElementById('image-container');
-    // Create a new Image element for the cat
-    var catImage = new Image();
-    // Set the source (file path) for the cat image
-    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
-    // Set alternative text for the image (for accessibility)
-    catImage.alt = 'Cat';
-    // When the cat image is fully loaded, add it to the image container
-    catImage.onload = function() {
-        imageContainer.appendChild(catImage);
-    };
-}
-
-// Display the cat.gif initially
-displayCat();
